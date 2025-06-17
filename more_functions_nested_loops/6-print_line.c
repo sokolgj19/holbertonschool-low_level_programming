2af6dd950338a8ee837ead4932dc3ae7e8d3575c
sokolgj19 <gjekasokol19@gmail.com>
2025-06-17 21:23:55 +0000

    Write a function that draws a straight line in the terminal using _putchar

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,21 +5,21 @@
  * @n: Number of times the character _ should be printed
  *
  * Description: Uses _putchar to print a line of underscores.
- *              If n <= 0, only a newline is printed.
+ * If n <= 0, only a newline is printed.
  */
 void print_line(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < n; i++)
-    {
-        _putchar('_');
-    }
-    _putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
