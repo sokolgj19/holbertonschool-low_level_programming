ae343311d3da2faad52e8296cb66c1c25f904eb5
sokolgj19 <gjekasokol19@gmail.com>
2025-07-24 11:01:54 +0000

    Write a program that copies the content of a file to another file.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_error - Prints error message and exits
- * @exit_code: code to exit with
- * @msg: error message
- * @arg: filename or fd
+ * print_error - Prints error and exits
+ * @exit_code: exit status
+ * @msg: message to print
+ * @arg: related filename or descriptor
  */
 void print_error(int exit_code, const char *msg, const char *arg)
 {
@@ -13,16 +13,16 @@ void print_error(int exit_code, const char *msg, const char *arg)
 }
 
 /**
- * main - Copies content of a file to another
- * @argc: number of arguments
+ * main - Copies file content from one file to another
+ * @argc: argument count
  * @argv: argument vector
  *
- * Return: 0 on success, exit codes on error
+ * Return: 0 on success, exits 97‑100 on error
  */
 int main(int argc, char *argv[])
 {
 	int from_fd, to_fd, rd, wr;
-	char buffer[1024];
+	char buf[1024];
 
 	if (argc != 3)
 	{
@@ -38,12 +38,15 @@ int main(int argc, char *argv[])
 	if (to_fd == -1)
 		print_error(99, "Error: Can't write to", argv[2]);
 
-	while ((rd = read(from_fd, buffer, 1024)) != 0)
+	while (1)
 	{
+		rd = read(from_fd, buf, 1024);
 		if (rd == -1)
 			print_error(98, "Error: Can't read from file", argv[1]);
+		if (rd == 0)
+			break;
 
-		wr = write(to_fd, buffer, rd);
+		wr = write(to_fd, buf, rd);
 		if (wr == -1 || wr != rd)
 			print_error(99, "Error: Can't write to", argv[2]);
 	}
